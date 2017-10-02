30114ef4511690a39d0a6ed6c3a1658e7eeed903
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-10-02 14:42:55 +0100

    Added print_statement method
    - The client can print the bank statement
    - Changed approach for the balance. Now "balance" is a key of the array of hashes "bank_account"
    - Tests for the statement and for the balance pass
    - Refactored the code and added comments

--- lib/bank.rb
+++ lib/bank.rb
@@ -4,17 +4,25 @@ class Bank
 
 	def initialize(balance = 0)
 		@bank_account = [] # <-- Client bank account
-		@balance = balance
+		@balance = balance # <-- Balance of the bank account
 	end
 
 	def deposit(date, deposit)
-		@bank_account << {date: date, deposit: deposit} # <-- A date and an amount for the deposit go into the client's bank account
-		@balance += deposit # <-- The current balance after a deposit
+		@bank_account << {date: date, deposit: deposit, balance: @balance += deposit}
+		# <-- A date and an amount for the deposit go into the client's bank account, increasing its balance
 	end
 
 	def withdrawal(date, withdrawal)
-		@bank_account << {date: date, withdrawal: withdrawal}# <-- A date and an amount for the withdrawal go into the client's bank account
-		@balance -= withdrawal # <-- The current balance after a withdrawal
+		@bank_account << {date: date, withdrawal: withdrawal, balance: @balance -= withdrawal}
+		# <-- A date and an amount for the withdrawal go into the client's bank account, decreasing its balance
+	end
+
+	def print_statement
+		puts "Date || Credit || Debit || Balance"
+		@bank_account.reverse_each do |n|
+  			puts "#{n[:date]} || #{n[:deposit]} || #{n[:withdrawal]} || #{n[:balance]}"
+		end
+		# <-- Prints the statament in a reverse order including "date", potential "deposits" and/or "withdrawals" and "balance"
 	end
 
 end
@@ -27,10 +35,13 @@ end
 
 
 bank = Bank.new
+p bank.balance
 bank.deposit("02-10-2017", 1000)
-bank.deposit("03-10-2017", 2000)
-p bank.bank_account
+# bank.deposit("03-10-2017", 2000)
+# p bank.bank_account
 p bank.balance
 bank.withdrawal("04-10-2017", 500)
-p bank.bank_account
 p bank.balance
+# p bank.bank_account
+# p bank.balance
+p bank.print_statement
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -4,14 +4,34 @@ describe Bank do
 	context "When testing the deposit method" do
 		it "should test that the client is able to make a deposit" do
 			subject.deposit("02-10-2017", 1000)
-			expect(subject.bank_account).to eq([{date: "02-10-2017", deposit: 1000}])
+			expect(subject.bank_account).to eq([{date: "02-10-2017", deposit: 1000, balance: 1000}])
 		end
 	end
 
 	context "When testing the withdrawal method" do
 		it "should test that the client is able to withdraw money" do
 			subject.withdrawal("04-10-2017", 500)
-			expect(subject.bank_account).to eq([{date: "04-10-2017", withdrawal: 500}])
+			expect(subject.bank_account).to eq([{date: "04-10-2017", withdrawal: 500, balance: -500}])
+		end
+	end
+
+	context "When testing the balance of the bank account" do
+		it "should print the correct balance" do
+			subject.deposit("02-10-2017", 1000)
+			subject.withdrawal("04-10-2017", 500)
+			expect(subject.balance).to eq(500)
+		end
+	end
+
+	context "When testing the print_statement method" do
+		it "should test that the client can access the statement" do
+			subject.deposit("02-10-2017", 1000)
+			subject.withdrawal("04-10-2017", 500)
+			expect{subject.print_statement}.to output(
+				"Date || Credit || Debit || Balance\n" +
+				"04-10-2017 ||  || 500 || 500\n" +
+				"02-10-2017 || 1000 ||  || 1000\n"
+				).to_stdout
 		end
 	end
 end
