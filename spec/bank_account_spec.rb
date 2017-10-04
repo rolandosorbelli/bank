dcbc8c7b6bf0ba110b1c705e7d9f83255f64971f
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-10-04 20:05:40 +0100

    Wrote some tests
    - Tested bank_account.rb
    - Refactored the code of the test with the new methods and variables

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,9 +1,8 @@
 require 'bank_statement'
 require 'bank_account'
+require 'bank_transaction'
 
 describe BankAccount do
-  deposit = [{ date: '02-10-2017', deposit: 1000, balance: 1000 }]
-  withdrawal = [{ date: '04-10-2017', withdraw: 500, balance: -500 }]
 
   context 'When testing the bank_account array' do
     it 'should test that the bank account is empty' do
@@ -14,21 +13,26 @@ describe BankAccount do
   context 'When testing the deposit method' do
     it 'should test that the client is able to make a deposit' do
       subject.deposit('02-10-2017', 1000)
-      expect(subject.transactions).to eq(deposit)
+      expect(subject.balance).to eq(1000)
     end
   end
 
   context 'When testing the withdrawal method' do
     it 'should test that the client is able to withdraw money' do
       subject.withdrawal('04-10-2017', 500)
-      expect(subject.transactions).to eq(withdrawal)
+      expect(subject.balance).to eq(-500)
     end
   end
 
   context 'When testing the balance of the bank account' do
-    it 'should print the correct balance' do
+    it 'should print the correct balance after a few transactions' do
       subject.deposit('02-10-2017', 1000)
-      expect(subject.balance).to eq(1000)
+      subject.withdrawal('04-10-2017', 500)
+      expect(subject.balance).to eq(500)
+    end
+
+    it 'should print the balance when the statement is empty' do
+      expect(subject.balance).to eq(0)
     end
   end
 end
--- spec/bank_feature_test.rb
+++ spec/bank_feature_test.rb
@@ -5,9 +5,5 @@ require '../lib/bank_transaction'
 bank = BankAccount.new
 statement = BankStatement.new
 
-bank.deposit('02-10-2017', 1000)
-bank.deposit('03-10-2017', 500)
-bank.withdrawal('04-10-2017', 200)
-p bank.transactions
-p bank.balance
-p statement.print_statement
+p bank.deposit('02-10-2017', 1000)
+# p bank.transactions
