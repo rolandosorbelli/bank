1a78344b4fa83365410ffbf43666e8152fdf3c27
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-10-05 13:01:39 +0100

    Tested all file
    - Coverage 100%

--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -2,25 +2,36 @@ require 'bank_statement'
 require 'bank_account'
 
 describe BankStatement do
-  let(:bank) { double(:bank) }
-  transaction = [{ date: '02-10-2017', deposit: 1000, balance: 1000 }]
+
+  let(:account) { double :account }
 
   context 'When testing the statement_headline method' do
     it 'should test that there is a headline in the statement' do
       expect { subject.print_statement }.to output(
         "Date || Credit || Debit || Balance\n"
-      ).to_stdout
+        ).to_stdout
     end
   end
 
   context 'When testing the statement output' do
-    it 'should test that a statement will be printed' do
-      allow(bank).to receive(:transactions).and_return(transaction)
-      subject = BankStatement.new(bank)
-      expect { subject.print_statement }.to output(
-        "Date || Credit || Debit || Balance\n" \
-        "02-10-2017 || 1000 ||  || 1000\n"
-      ).to_stdout
+    it 'should print a deposit transaction' do
+      account = BankAccount.new
+      statement = BankStatement.new(account)
+      account.deposit("01-10-2017", 500)
+      expect { statement.print_statement }.to output(
+        "Date || Credit || Debit || Balance\n" +
+        "01-10-2017 || 500.00 || || 500.00\n"
+        ).to_stdout
+    end
+
+    it 'should print a withdrawal transaction' do
+      account = BankAccount.new
+      statement = BankStatement.new(account)
+      account.withdrawal("01-10-2017", 500)
+      expect { statement.print_statement }.to output(
+        "Date || Credit || Debit || Balance\n" +
+        "01-10-2017 || || 500.00 || -500.00\n"
+        ).to_stdout
     end
   end
 end
