3294807132d538378fcba5697b5540467c7aacdc
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-10-05 13:11:51 +0100

    0 rubocop offenses

--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -2,36 +2,31 @@ require 'bank_statement'
 require 'bank_account'
 
 describe BankStatement do
-
-  let(:account) { double :account }
-
-  context 'When testing the statement_headline method' do
+  context 'When testing the statement output' do
     it 'should test that there is a headline in the statement' do
       expect { subject.print_statement }.to output(
         "Date || Credit || Debit || Balance\n"
-        ).to_stdout
+      ).to_stdout
     end
-  end
 
-  context 'When testing the statement output' do
     it 'should print a deposit transaction' do
       account = BankAccount.new
       statement = BankStatement.new(account)
-      account.deposit("01-10-2017", 500)
+      account.deposit('01-10-2017', 500)
       expect { statement.print_statement }.to output(
-        "Date || Credit || Debit || Balance\n" +
+        "Date || Credit || Debit || Balance\n" \
         "01-10-2017 || 500.00 || || 500.00\n"
-        ).to_stdout
+      ).to_stdout
     end
 
     it 'should print a withdrawal transaction' do
       account = BankAccount.new
       statement = BankStatement.new(account)
-      account.withdrawal("01-10-2017", 500)
+      account.withdrawal('01-10-2017', 500)
       expect { statement.print_statement }.to output(
-        "Date || Credit || Debit || Balance\n" +
+        "Date || Credit || Debit || Balance\n" \
         "01-10-2017 || || 500.00 || -500.00\n"
-        ).to_stdout
+      ).to_stdout
     end
   end
 end
